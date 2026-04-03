6a34ab699b56ec10e65fdb7c54580763c8951f69
Drishti1501 <drishtinarang1@gmail.com>
2026-04-03 22:26:13 +0530

    Update main.java

--- main.java
+++ main.java
@@ -30,3 +30,38 @@ class Food implements Serializable
         }
     }
 }
+class Singleroom implements Serializable
+{
+    String name;
+    String contact;
+    String gender;   
+    ArrayList<Food> food =new ArrayList<>();
+
+   
+    Singleroom()
+    {
+        this.name="";
+    }
+    Singleroom(String name,String contact,String gender)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+    }
+}
+class Doubleroom extends Singleroom implements Serializable
+{ 
+    String name2;
+    String contact2;
+    String gender2;  
+    
+    Doubleroom()
+    {
+        this.name="";
+        this.name2="";
+    }
+    Doubleroom(String name,String contact,String gender,String name2,String contact2,String gender2)
+    {
+        this.name=name;
+        this.contact=contact;
+        
