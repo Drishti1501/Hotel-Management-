6523357a28662ee0a3caafd07a977aa8b0399625
Drishti1501 <drishtinarang1@gmail.com>
2026-04-03 22:27:23 +0530

    Update main.java

--- main.java
+++ main.java
@@ -64,4 +64,27 @@ class Doubleroom extends Singleroom implements Serializable
     {
         this.name=name;
         this.contact=contact;
-        
+        this.gender=gender;
+        this.name2=name2;
+        this.contact2=contact2;
+        this.gender2=gender2;
+    }
+}
+class NotAvailable extends Exception
+{
+    @Override
+    public String toString()
+    {
+        return "Not Available !";
+    }
+}
+
+class holder implements Serializable
+{
+    Doubleroom luxury_doublerrom[]=new Doubleroom[10]; //Luxury
+    Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; //Deluxe
+    Singleroom luxury_singleerrom[]=new Singleroom[10]; //Luxury
+    Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
+}
+
+    
